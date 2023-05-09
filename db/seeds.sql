83580fcc8fe24e19ea9f3d3e7e3c2c1ceff9136f
jkrieger6 <jkrieger11@gmail.com>
2023-05-09 15:54:48 -0700

    added seeds to database but didnt insert them yet

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (id)
+VALUES ('Sales'), 
+('Engineering'), 
+('Finance'), 
+('Legal');
+
+INSERT INTO role (title,salary,id)
+VALUES ('Sales Lead' ,100000,1),
+('Salesperson',80000,1),
+('Lead Engineer',150000,2),
+('Software Engineer',120000,2),
+('Accountant',125000,3),
+('Legal Team Lead',250000,4),
+('Lawyer',190000,4);
+
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ('Janette' ,'Dough',1,NULL),
+('John','Smith',2,1),
+('Molly','Lowe',3,NULL),
+('Jake','Krieger',4,3),
+('Rupert','Thor',5,3),
+('Lightning','McQueen',6,NULL),
+('Sarah','Brown',7,6);
+    
