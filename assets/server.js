d54544f0e1e7b9f2a123c6f3a8e765a25fcc4af2
jkrieger6 <jkrieger11@gmail.com>
2023-05-10 09:37:18 -0700

    added routes to get employee database, role tables, add employee and add role

--- assets/server.js
+++ assets/server.js
@@ -44,25 +44,23 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: 'password',
-        database: 'employee_db'
+        database: 'employee_trackerDB'
     },
-    console.log(`Connected to the employee_db database.`)
+    console.log(`Connected to the employee_tracker database.`)
 );
 
 // Start server after DB connection
 db.connect(err => {
     if (err) throw err;
-    console.log('Database connected.');
+    console.log('Database not connected.');
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
-}
-);
+});
 
 // View all departments
 app.get('/api/departments', (req, res) => {
-    const sql = `SELECT * FROM department`;
-    db.query(sql, (err, rows) => {
+    db.query(`SELECT * FROM employee_trackerDB`, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
@@ -72,8 +70,109 @@ app.get('/api/departments', (req, res) => {
             data: rows
         });
     });
-}
-);
+});
+
+// View all roles
+app.get('/api/roles', (req, res) => {
+    db.query('SELECT * FROM roles', (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// View all employees
+app.get('/api/employees', (req, res) => {
+    db.query('SELECT * FROM employees', (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Add a department
+app.post('/api/departments', ({ body }, res) => {
+    let sql = `INSERT INTO departments (name) VALUES (?)`;
+    let params = [body.name];
+
+    db.query(sql, params, (err, res) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// Add a role
+app.post('/api/roles', ({ body }, res) => {
+    let sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+    let params = [body.title, body.salary, body.department_id];
+
+    db.query(sql, params, (err, res) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// Add an employee
+app.post('/api/employees', ({ body }, res) => {
+    let sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id)`;
+    let params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+
+    db.query(sql, params, (err, res) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// Update a current employee's role
+app.put('/api/employees/:id', (req, res) => {
+    let sql = `UPDATE employee SET role_id =? WHERE id = ?`;
+    let params = [req.body.role_id, req.params.id];
+
+    db.query(sql, params, (err, res) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!res.affectedRows) {
+            res.json({
+                message: 'Employee not found.'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: res.affectedRows
+            });
+        }
+    });
+});
+
 
 // Listener to connect to server
 app.listen(PORT, () => {
