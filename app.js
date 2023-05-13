176ad9d1e52bba26e256c0ffdce9e621ce02bb69
jkrieger6 <jkrieger11@gmail.com>
2023-05-13 11:06:27 -0700

    added ability to update employee manager

--- app.js
+++ app.js
@@ -19,9 +19,8 @@ const db = mysql.createConnection(
 );
 promptUser();
 
-
-function promptUser () {
- inquirer
+function promptUser() {
+  inquirer
     .prompt([
       {
         type: "list",
@@ -35,6 +34,7 @@ function promptUser () {
           "Add a role",
           "Add an employee",
           "Update an employee role",
+          "Update an employee manager",
           "Exit",
         ],
       },
@@ -55,11 +55,13 @@ function promptUser () {
         addEmployee();
       } else if (options === "Update an employee role") {
         updateEmployeeRole();
+      } else if (options === "Update an employee manager") {
+        updateEmployeeManager();
       } else if (options === "Exit") {
         db.end();
       }
     });
-};
+}
 
 function viewDepartments() {
   db.query("SELECT * FROM department", function (err, results) {
@@ -69,142 +71,183 @@ function viewDepartments() {
 }
 
 // // View all roles
-function viewRoles () {
-    db.query("SELECT * FROM role", function (err, results) {
-        console.table(results);
-        promptUser();
+function viewRoles() {
+  db.query("SELECT * FROM role", function (err, results) {
+    console.table(results);
+    promptUser();
   });
 }
 
 // // View all employees
-function viewEmployees () {
-    db.query("SELECT employee.id, employee.first_name, employee.last_name FROM employee JOIN role ON employee.role_id = role_id", function (err, results) {
-        console.table(results);
-        promptUser();
-});
+function viewEmployees() {
+  db.query(
+    "SELECT employee.id, employee.first_name, employee.last_name FROM employee JOIN role ON employee.role_id = role_id",
+    function (err, results) {
+      console.table(results);
+      promptUser();
+    }
+  );
 }
 
 // Add a department
-function addDepartment () {
-    inquirer.prompt ({
-        type: "input",
-        name: "department",
-        message: "Please enter a name for new department"
-    }) 
-    .then ((answer) => {
-        let sql = `INSERT INTO department (name) VALUES (?)`;
-        let params = [answer.department];
-      
-        db.query(sql, params, (err, res) => {
-          if (err) {
-            console.log("Could not add department.");
-          } else {
-            viewDepartments();
-          }
-          promptUser();
-        });
-    });   
+function addDepartment() {
+  inquirer
+    .prompt({
+      type: "input",
+      name: "department",
+      message: "Please enter a name for new department",
+    })
+    .then((answer) => {
+      let sql = `INSERT INTO department (name) VALUES (?)`;
+      let params = [answer.department];
+
+      db.query(sql, params, (err, res) => {
+        if (err) {
+          console.log("Could not add department.");
+        } else {
+          viewDepartments();
+        }
+        promptUser();
+      });
+    });
 }
 
 // Add a role
-function addRole () {
-    inquirer.prompt ([
-        {
-            type: "input",
-            name: "title",
-            message: "Please enter a title for new role"
-        },
-        {
-            type: "input",
-            name: "salary",
-            message: "Please enter a salary for new role"
-        },
-        {
-            type: "input",
-            name: "department_id",
-            message: "Please enter a department ID for new role"
-        }
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Please enter a title for new role",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Please enter a salary for new role",
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "Please enter a department ID for new role",
+      },
     ])
-    .then ((answer) => {
-        let sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
-        let params = [answer.title, answer.salary, answer.department_id];
+    .then((answer) => {
+      let sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+      let params = [answer.title, answer.salary, answer.department_id];
 
-        db.query(sql, params, (err, res) => {
-            if (err) {
-                console.log("Could not add role.");
-            } else {
-                viewRoles();
-            }
-            promptUser();
-        });
+      db.query(sql, params, (err, res) => {
+        if (err) {
+          console.log("Could not add role.");
+        } else {
+          viewRoles();
+        }
+        promptUser();
+      });
     });
 }
 
 // // Add an employee
-function addEmployee () {
-    inquirer.prompt ([
-        {
-            type: "input",
-            name: "first_name",
-            message: "Please enter a first name for new employee"
-        },
-        {
-            type: "input",
-            name: "last_name",
-            message: "Please enter a last name for new employee"
-        },
-        {
-            type: "input",
-            name: "role_id",
-            message: "Please enter a role ID for new employee"
-        },
-        {
-            type: "input",
-            name: "manager_id",
-            message: "Please enter a manager ID for new employee"
-        }
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "Please enter a first name for new employee",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Please enter a last name for new employee",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "Please enter a role ID for new employee",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "Please enter a manager ID for new employee",
+      },
     ])
-    .then ((answer) => {
-        let sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-        let params = [answer.first_name, answer.last_name, answer.role_id, answer.manager_id];
+    .then((answer) => {
+      let sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+      let params = [
+        answer.first_name,
+        answer.last_name,
+        answer.role_id,
+        answer.manager_id,
+      ];
 
-        db.query(sql, params, (err, res) => {
-            if (err) {
-                console.log("Could not add employee.");
-            } else {
-                viewEmployees();
-            }
-            promptUser();
-        });
+      db.query(sql, params, (err, res) => {
+        if (err) {
+          console.log("Could not add employee.");
+        } else {
+          viewEmployees();
+        }
+        promptUser();
+      });
     });
 }
 
 // // Update a current employee's role
-function updateEmployeeRole () {
-    inquirer.prompt ([
-        {
-            type: "input",
-            name: "employee_id",
-            message: "Please enter an employee ID to update"
-        },
-        {
-            type: "input",
-            name: "role_id",
-            message: "Please enter a new role ID for employee"
-        }
+function updateEmployeeRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "employee_id",
+        message: "Please enter an employee ID to update",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "Please enter a new role ID for employee",
+      },
     ])
-    .then ((answer) => {
-        let sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
-        let params = [answer.role_id, answer.employee_id];
+    .then((answer) => {
+      let sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+      let params = [answer.role_id, answer.employee_id];
 
-        db.query(sql, params, (err, res) => {
-            if (err) {
-                console.log("Could not update employee role.");
-            } else {
-                viewEmployees();
-            }
-            promptUser();
-        });
+      db.query(sql, params, (err, res) => {
+        if (err) {
+          console.log("Could not update employee role.");
+        } else {
+          viewEmployees();
+        }
+        promptUser();
+      });
     });
 }
 
+// Function to update employee manager
+function updateEmployeeManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "employee_id",
+        message: "Please enter an employee ID to update",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "Please enter a new manager ID for employee",
+      },
+    ])
+    .then((answer) => {
+      let sql = `UPDATE employee SET manager_id = ? WHERE id = ?`;
+      let params = [answer.manager_id, answer.employee_id];
+
+      db.query(sql, params, (err, res) => {
+        if (err) {
+          console.log("Could not update employee manager.");
+        } else {
+          viewEmployees();
+        }
+        promptUser();
+      });
+    });
+}
