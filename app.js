15e196a2225e59881d87ce85e6a7f27bcf6d1313
jkrieger6 <jkrieger11@gmail.com>
2023-05-14 07:37:03 -0700

    added sql join to show all employees

--- app.js
+++ app.js
@@ -81,7 +81,14 @@ function viewRoles() {
 // // View all employees
 function viewEmployees() {
   db.query(
-    "SELECT employee.id, employee.first_name, employee.last_name FROM employee JOIN role ON employee.role_id = role_id",
+    `SELECT employee.id, employee.first_name, employee.last_name, 
+role.title, department.name AS department, role.salary, 
+    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id`,
+
     function (err, results) {
       console.table(results);
       promptUser();
